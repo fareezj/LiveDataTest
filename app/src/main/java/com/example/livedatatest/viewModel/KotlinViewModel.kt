4333e0fd6991b69b0bb7452b46e31969e0a0c24b
Fareez <fareez.mohamad@hexainitiative.com>
2021-01-07 15:41:48 +0800

    Add Mediator Live Data example

--- app/src/main/java/com/example/livedatatest/viewModel/KotlinViewModel.kt
+++ app/src/main/java/com/example/livedatatest/viewModel/KotlinViewModel.kt
@@ -2,20 +2,37 @@ package com.example.livedatatest.viewModel
 
 import android.util.Log
 import android.view.View
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 
 class KotlinViewModel: ViewModel() {
 
     private val _secretNumber = MutableLiveData<String>()
     val secretNumber = _secretNumber as LiveData<String>
 
+    private val _primeNumber = MutableLiveData<String>()
+    val primeNumber: LiveData<String> = _primeNumber
+
     var num = 1
     var numFinal = 0
 
     init {
-        _secretNumber.value = "0"
+        _secretNumber.value = "HELLLO"
+        _primeNumber.value = "1"
+    }
+
+    val isLoading: LiveData<String> = combinedLiveData(listOf(_secretNumber, _primeNumber))
+
+    private fun combinedLiveData(inputs: List<LiveData<String>>): LiveData<String> {
+
+        val result = MediatorLiveData<String>()
+
+        val doCalculation = Observer<String> {
+            result.value = inputs.any { it.value == "HELLLO" }.toString()
+        }
+        inputs.forEach {
+            result.addSource(it, doCalculation)
+        }
+        return result
     }
 
     fun addNum(view: View) {
